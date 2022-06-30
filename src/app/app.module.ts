180648a7cf6b1d49e3fb30acba1d5a439d285a2b
unknown <qquangmminh19999@gmail.com>
2022-06-30 16:17:02 +0700

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
-import {FormsModule} from "@angular/forms";
-import { AngularShowPetInfoComponent } from './angular-show-pet-info/angular-show-pet-info.component';
+
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FontSizeEditorComponent,
-    AngularShowPetInfoComponent
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pet.ts
+++
@@ -1,5 +0,0 @@
-export interface Pet {
-  name?: string;
-  image?: string;
-  // Trong đó sử dụng ?: để khai báo thuộc tính optional cho interface
-}
