603ec48166711384cdd5a0862f77ee9c4fb6284b
unknown <qquangmminh19999@gmail.com>
2022-07-04 10:16:41 +0700

    demoFormReactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'minh';
+  countryList = [
+    {id: 1, name: 'Viet Nam'},
+    {id: 2, name: 'Laos'},
+    {id: 3, name: 'Indonesia'}
+  ];
+  contactForm = new FormGroup({
+    firstname: new FormControl(),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+  });
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
