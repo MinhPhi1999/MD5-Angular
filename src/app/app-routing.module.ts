725337a0d64f66ce6e68c90de1b209d1940aea80
unknown <qquangmminh19999@gmail.com>
2022-07-05 16:34:03 +0700

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserComponent} from './user/user.component';
+import {AdminComponent} from './admin/admin.component';
+import {DetailComponent} from './user/detail/detail.component';
+import {CreateComponent} from './user/create/create.component';
+import {DeleteComponent} from './user/detail/delete/delete.component';
+import {LoginComponent} from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: 'detail',
+        component: DetailComponent,
+        children: [
+          {
+            path: 'delete',
+            component: DeleteComponent
+          }
+        ]
+      },
+      {
+        path: 'create',
+        component: CreateComponent
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { DetailComponent } from './user/detail/detail.component';
+import { CreateComponent } from './user/create/create.component';
+import { DeleteComponent } from './user/detail/delete/delete.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    DetailComponent,
+    CreateComponent,
+    DeleteComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
