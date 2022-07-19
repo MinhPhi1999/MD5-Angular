ea522cfc964baf73a53bcf82a12c3eb1d73deb64
unknown <qquangmminh19999@gmail.com>
2022-07-19 09:37:18 +0700

    minhhhhhhhhhhhhh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
